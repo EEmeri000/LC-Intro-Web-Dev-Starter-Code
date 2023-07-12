b5ad7c00b34463a97a8838af7dc9390036d398ec
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-07-12 11:53:28 -0400

    Added Chapter 12 Exercises

--- chapter12/exercises/e12.js
+++ chapter12/exercises/e12.js
@@ -2,14 +2,42 @@ let superChimpOne = {
     name: "Chad",
     species: "Chimpanzee",
     mass: 9,
-    age: 6
+    age: 6,
+    astronautID: 1,
+    move: function () {return Math.floor(Math.random()*11)}
 };
 
 let salamander = {
     name: "Lacey",
     species: "Axolotl Salamander",
     mass: 0.1,
-    age: 5
+    age: 5,
+    astronautID: 2,
+    move: function () {return Math.floor(Math.random()*11)}
+};
+let superChimpTwo = {
+    name: "Brad",
+    species: "Chimpanzee",
+    mass: 11,
+    age: 6,
+    astronautID: 3,
+    move: function () {return Math.floor(Math.random()*11)}
+};
+let beagle = {
+    name: "Leroy",
+    species: "Beagle",
+    mass: 14,
+    age: 5,
+    astronautID: 4,
+    move: function () {return Math.floor(Math.random()*11)}
+};
+let tardigrade = {
+    name: "Almina",
+    species: "Tardigrade",
+    mass: 0.0000000001,
+    age: 1,
+    astronautID: 5,
+    move: function () {return Math.floor(Math.random()*11)}
 };
 
 
@@ -17,6 +45,33 @@ let salamander = {
 
 // Create an array to hold the animal objects.
 
+let crew = [superChimpOne, superChimpTwo, salamander, beagle, tardigrade];
+
 // Print out the relevant information about each animal.
 
+
+console.log(crew)
+
+function crewReports(crewMember) {
+    console.log(`'${crewMember.name} is a ${crewMember.species}. They are ${crewMember.age} years old and ${crewMember.mass} kilograms. Their ID is ${crewMember.astronautID}.'`)
+ 
+}
+console.log(crewReports(salamander))
 // Start an animal race!
+
+
+function fitnessTest(candidates){
+    let results = [], numSteps, turns;
+    for (let i = 0; i < candidates.length; i++){
+        numSteps = 0;
+        turns = 0;
+        while(numSteps < 20){
+        numSteps += candidates[i].move();
+        turns++;
+        }
+        results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+    }
+    return results;
+   }
+
+   console.log(fitnessTest(crew))
