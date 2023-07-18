8260c7bc13e29fce53313fa5d4014f55da63e69a
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-07-18 11:18:06 -0400

    partway through error exercises

--- chapter17/exercises/e17-2.js
+++ chapter17/exercises/e17-2.js
@@ -5,7 +5,7 @@ function gradeLabs(labs) {
       let lab = labs[i];
       let result = lab.runLab(3);
       console.log(`${lab.student} code worked: ${result === 27}`);
-    }
+      }
   }
   
   let studentLabs = [
@@ -22,5 +22,28 @@ function gradeLabs(labs) {
       }
     }
   ];
-  
+
   gradeLabs(studentLabs);
+
+  let studentLabs2 = [
+   {
+      student: 'Blake',
+      myCode: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Jessica',
+      runLab: function (num) {
+         return Math.pow(num, num);
+      }
+   },
+   {
+      student: 'Mya',
+      runLab: function (num) {
+         return num * num;
+      }
+   }
+];
+
+gradeLabs(studentLabs2)
