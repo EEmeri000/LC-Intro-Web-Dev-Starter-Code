4ed57b669a41a19b939c11acf79d35dfdd5bcb5d
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-07-18 11:28:16 -0400

    fixed it!

--- chapter17/exercises/e17-2.js
+++ chapter17/exercises/e17-2.js
@@ -2,12 +2,16 @@
 
 function gradeLabs(labs) {
     for (let i=0; i < labs.length; i++) {
-      let lab = labs[i];
+      let lab = labs[i]; 
+      try { 
       let result = lab.runLab(3);
       console.log(`${lab.student} code worked: ${result === 27}`);
+      } catch(err) { 
+         result = "Error Thrown"
+         console.log(result) 
       }
   }
-  
+}
   let studentLabs = [
     {
       student: 'Carly',
